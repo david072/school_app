43bffd41da390050e1a39b4d8b9a357292010ead
david <david.g.ganz@gmail.com>
2022-06-25 23:23:28 +0200

    feat(BackgroundWorker): Add notification action
    Each notification emitted by the BackgroundWorker now has an action to
    mark the corresponding task as completed.

--- lib/util/app_notifications.dart
+++ lib/util/app_notifications.dart
@@ -1,8 +1,12 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:school_app/background_worker.dart';
 
 class AppNotifications {
+  static const _markTaskCompletedActionKey = 'mark-task-completed-action';
+  static const _taskIdPayloadKey = 'taskId';
+
   static const _taskNotificationsChannelGroupKey =
       'task_notifications_channel_group';
   static const _taskNotificationsChannelKey = 'task_notifications';
@@ -12,6 +16,14 @@ class AppNotifications {
     final taskNotificationsChannelName = 'notification_channel_name'.tr;
     final taskNotificationsChannelDescription =
         'notification_channel_description'.tr;
+
+    AwesomeNotifications().actionStream.listen((event) {
+      if (event.payload == null) return;
+      if (event.payload![_taskIdPayloadKey] == null) return;
+
+      BackgroundWorker.markTaskCompleted(event.payload![_taskIdPayloadKey]!);
+      AwesomeNotifications().dismiss(event.id!);
+    });
 
     AwesomeNotifications().initialize(
       null,
@@ -33,14 +45,22 @@ class AppNotifications {
     );
   }
 
-  static Future<void> createNotification(String title, String body) async {
+  static Future<void> createTaskNotification(
+      String taskId, String title, String body) async {
     await AwesomeNotifications().createNotification(
       content: NotificationContent(
         id: _getNotificationId(),
         channelKey: _taskNotificationsChannelKey,
         title: title,
         body: body,
+        payload: {_taskIdPayloadKey: taskId},
       ),
+      actionButtons: [
+        NotificationActionButton(
+          key: _markTaskCompletedActionKey,
+          label: 'notification_action_mark_completed'.tr,
+        ),
+      ],
     );
   }
 
