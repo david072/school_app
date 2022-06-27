28f48cd402c6867c760b897e2b2a902529712b54
david <david.g.ganz@gmail.com>
2022-06-27 20:42:39 +0200

    refactor(SoonTasksWidget): Extract table into TasksList widget

--- lib/pages/tasks/soon_tasks_widget.dart
+++ lib/pages/tasks/soon_tasks_widget.dart
@@ -11,6 +11,175 @@ import 'package:school_app/pages/tasks/view_task_page.dart';
 import 'package:school_app/util/sizes.dart';
 import 'package:school_app/util/util.dart';
 
+class TasksList extends StatefulWidget {
+  const TasksList({
+    Key? key,
+    required this.tasks,
+  }) : super(key: key);
+
+  final List<Task> tasks;
+
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  late Offset longPressPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: GestureDetector(
+              onTapDown: (details) =>
+                  longPressPosition = details.globalPosition,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: constraints.minWidth),
+                child: DataTable(
+                  columnSpacing: isSmallScreen(context) ? 20 : null,
+                  showCheckboxColumn: false,
+                  columns: [
+                    DataColumn(label: Text('done'.tr)),
+                    DataColumn(label: Text('due'.tr)),
+                    DataColumn(label: Text('title'.tr)),
+                    DataColumn(label: Text('subject'.tr)),
+                  ],
+                  rows: widget.tasks
+                      .map(
+                        (task) => _taskRow(
+                          context,
+                          task,
+                          () => showPopupMenu(
+                            context: context,
+                            items: [
+                              PopupMenuItem(
+                                value: 0,
+                                child: Text('edit'.tr),
+                              ),
+                              PopupMenuItem(
+                                value: 1,
+                                child: Text('delete'.tr),
+                              ),
+                            ],
+                            longPressPosition: longPressPosition,
+                            functions: [
+                              () => Get.to(
+                                  () => CreateTaskPage(taskToEdit: task)),
+                              () => showConfirmationDialog(
+                                    context: context,
+                                    title: 'delete'.tr,
+                                    content: 'delete_task_confirm'
+                                        .trParams({'name': task.title}),
+                                    cancelText: 'cancel_caps'.tr,
+                                    confirmText: 'delete_caps'.tr,
+                                    onConfirm: () =>
+                                        Database.I.deleteTask(task.id),
+                                  ),
+                            ],
+                          ),
+                          () => Get.to(() => ViewTaskPage(taskId: task.id)),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  DataRow _taskRow(BuildContext context, Task task, void Function() onLongPress,
+      void Function() onSelectChanged) {
+    return DataRow(
+      color: MaterialStateProperty.resolveWith((states) {
+        if (task.completed) {
+          if (!Get.isDarkMode) {
+            return Colors.grey.shade300;
+          } else {
+            return Colors.grey.shade800;
+          }
+        }
+        return null;
+      }),
+      cells: [
+        DataCell(
+          Builder(
+            builder: (_) {
+              // (HACK) to make the UI element update instantly
+              var value = task.completed;
+              return StatefulBuilder(builder: (context, setState) {
+                return SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: Checkbox(
+                    onChanged: (b) {
+                      if (b == null) return;
+                      setState(() => value = b);
+                      Database.I.updateTaskStatus(task.id, b);
+                    },
+                    value: value,
+                  ),
+                );
+              });
+            },
+          ),
+        ),
+        DataCell(Text(task.formatRelativeDueDate(),
+            style: Theme.of(context).textTheme.bodyLarge)),
+        DataCell(
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: task.title,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                WidgetSpan(
+                    child: task.description.isNotEmpty
+                        ? const SizedBox(width: 10)
+                        : Container()),
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: task.description.isNotEmpty
+                      ? Icon(Icons.sticky_note_2_outlined,
+                          color: Theme.of(context).hintColor)
+                      : Container(),
+                )
+              ],
+            ),
+          ),
+        ),
+        DataCell(
+          RichText(
+            text: TextSpan(
+              children: [
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Icon(Icons.book_outlined, color: task.subject.color),
+                ),
+                const WidgetSpan(child: SizedBox(width: 10)),
+                TextSpan(
+                  text: task.subject.name,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: task.subject.color,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+      onLongPress: onLongPress,
+      onSelectChanged: (_) => onSelectChanged(),
+    );
+  }
+}
+
 class TaskListWidget extends StatefulWidget {
   const TaskListWidget({
     Key? key,
@@ -60,71 +229,7 @@ class _TaskListWidgetState extends State<TaskListWidget> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: LayoutBuilder(
-              builder: (context, constraints) => SingleChildScrollView(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: GestureDetector(
-                    onTapDown: (details) =>
-                        longPressPosition = details.globalPosition,
-                    child: ConstrainedBox(
-                      constraints:
-                          BoxConstraints(minWidth: constraints.minWidth),
-                      child: DataTable(
-                        columnSpacing: isSmallScreen(context) ? 20 : null,
-                        showCheckboxColumn: false,
-                        columns: [
-                          DataColumn(label: Text('done'.tr)),
-                          DataColumn(label: Text('due'.tr)),
-                          DataColumn(label: Text('title'.tr)),
-                          DataColumn(label: Text('subject'.tr)),
-                        ],
-                        rows: tasks
-                            .map(
-                              (task) => _taskRow(
-                                context,
-                                task,
-                                () => showPopupMenu(
-                                  context: context,
-                                  items: [
-                                    PopupMenuItem(
-                                      value: 0,
-                                      child: Text('edit'.tr),
-                                    ),
-                                    PopupMenuItem(
-                                      value: 1,
-                                      child: Text('delete'.tr),
-                                    ),
-                                  ],
-                                  longPressPosition: longPressPosition,
-                                  functions: [
-                                    () => Get.to(
-                                        () => CreateTaskPage(taskToEdit: task)),
-                                    () => showConfirmationDialog(
-                                      context: context,
-                                          title: 'delete'.tr,
-                                          content: 'delete_task_confirm'
-                                              .trParams({'name': task.title}),
-                                          cancelText: 'cancel_caps'.tr,
-                                          confirmText: 'delete_caps'.tr,
-                                          onConfirm: () =>
-                                              Database.I.deleteTask(task.id),
-                                        ),
-                                  ],
-                                ),
-                                () =>
-                                    Get.to(() => ViewTaskPage(taskId: task.id)),
-                              ),
-                            )
-                            .toList(),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
+          TasksList(tasks: tasks),
           Footer(
             reverse: widget.isHorizontal ? true : false,
             displayName: 'tasks'.tr,
@@ -136,90 +241,4 @@ class _TaskListWidgetState extends State<TaskListWidget> {
       ),
     );
   }
-}
-
-DataRow _taskRow(BuildContext context, Task task, void Function() onLongPress,
-    void Function() onSelectChanged) {
-  return DataRow(
-    color: MaterialStateProperty.resolveWith((states) {
-      if (task.completed) {
-        if (!Get.isDarkMode) {
-          return Colors.grey.shade300;
-        } else {
-          return Colors.grey.shade800;
-        }
-      }
-      return null;
-    }),
-    cells: [
-      DataCell(
-        Builder(
-          builder: (_) {
-            // (HACK) to make the UI element update instantly
-            var value = task.completed;
-            return StatefulBuilder(builder: (context, setState) {
-              return SizedBox(
-                width: 20,
-                height: 20,
-                child: Checkbox(
-                  onChanged: (b) {
-                    if (b == null) return;
-                    setState(() => value = b);
-                    Database.I.updateTaskStatus(task.id, b);
-                  },
-                  value: value,
-                ),
-              );
-            });
-          },
-        ),
-      ),
-      DataCell(Text(task.formatRelativeDueDate(),
-          style: Theme.of(context).textTheme.bodyLarge)),
-      DataCell(
-        RichText(
-          text: TextSpan(
-            children: [
-              TextSpan(
-                text: task.title,
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              WidgetSpan(
-                  child: task.description.isNotEmpty
-                      ? const SizedBox(width: 10)
-                      : Container()),
-              WidgetSpan(
-                alignment: PlaceholderAlignment.middle,
-                child: task.description.isNotEmpty
-                    ? Icon(Icons.sticky_note_2_outlined,
-                        color: Theme.of(context).hintColor)
-                    : Container(),
-              )
-            ],
-          ),
-        ),
-      ),
-      DataCell(
-        RichText(
-          text: TextSpan(
-            children: [
-              WidgetSpan(
-                alignment: PlaceholderAlignment.middle,
-                child: Icon(Icons.book_outlined, color: task.subject.color),
-              ),
-              const WidgetSpan(child: SizedBox(width: 10)),
-              TextSpan(
-                text: task.subject.name,
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: task.subject.color,
-                    ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ],
-    onLongPress: onLongPress,
-    onSelectChanged: (_) => onSelectChanged(),
-  );
 }
